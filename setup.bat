49e86150493dadc7209bfdc11ef7486ec3a0eb8c
ocha <ocha127@gmail.com>
2019-02-19 16:09:42 +0900

    シンボリックリンクのパスが通っていた無かったのを修正

--- setup.bat
+++ setup.bat
@@ -1,13 +1,13 @@
 @echo off
 cd /d %~dp0
-set MRUBYC_SRC=mrubyc\src
-echo %MRUBYC_SRC%\hal_posix
+set MRUBYC_SRC=%CD%\mrubyc\src
 git submodule update -i
 rd /s /q %MRUBYC_SRC%\hal_posix %MRUBYC_SRC%\hal_psoc5lp
 move %MRUBYC_SRC%\hal_esp32 %MRUBYC_SRC%\hal
 
-mklink /D %MRUBYC_SRC%\ext ..\..\ext\ext
-mklink %MRUBYC_SRC%\libmrubyc_config.h ..\..\ext\libmrubyc_config.h
-mklink %MRUBYC_SRC%\mrubyc_for_ESP32_Arduino.h ..\..\ext\mrubyc_for_ESP32_Arduino.h
-mklink /D src %MRUBYC_SRC%
+mklink /D %MRUBYC_SRC%\ext %CD%\ext\ext
+mklink %MRUBYC_SRC%\libmrubyc_config.h %CD%\ext\libmrubyc_config.h
+mklink %MRUBYC_SRC%\mrubyc_for_ESP32_Arduino.h %CD%\ext\mrubyc_for_ESP32_Arduino.h
+mklink /D %CD%\src %MRUBYC_SRC%
+
 
